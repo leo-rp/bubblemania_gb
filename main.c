d5e459665d3238167d8762c2b89af7468c8ae438
leo_rp <leorock1988@hotmail.com>
2019-10-13 01:22:08 -0500

    bubble monkey

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <string.h>
 #include <stdio.h>
 
-#include "logo.c"
-#include "graphics/face.c"
-#include "graphics/map.c"
-#include "graphics/maptiles.c"
+#include "graphics/oldrobotto.h"
+#include "graphics/player.c"
+#include "graphics/water.c"
+#include "graphics/background.h"
+
 #include "libs/carillon_funcs.c"
 
 
@@ -29,15 +30,15 @@
 
 
 
-INT8 game_status;
-INT16 xpos, ypos; 
-INT8 ysp, xsp, gravity, jump_force, shoot_force, speed_movement, player_direction, animate, soffset;
+INT8 game_status, game_counter, game_animation;
+INT16 xpos, ypos, ypos2, water_animation; 
+INT8 ysp, xsp, gravity, jump_force, shoot_force, speed_movement, player_direction, animate, soffset, water_delay;
 INT16 sensor_a, sensor_b;
 
 
-UINT8 sflip, sindex, lshoot, rshoot, dshoot;
+UINT8 sflip, sprite_index, lshoot, rshoot, dshoot;
 UINT8 onGround, can_jump;
-UINT8 stepCounter = 0;
+UINT8 game_play_counter = 0;
 UBYTE oldjoystate, joystate;
 UINT8 i;
 INT16 index, index2;
@@ -45,14 +46,6 @@ INT16 index, index2;
 UBYTE ix, iy, counter, offset, win;
 UINT16 p1, p2;
 
-
-struct Sprite {
-  UBYTE x;
-  UBYTE y;
-  UBYTE active;
-};
-
-struct Sprite sprites[40];
 
 struct Sprite {
   UBYTE x;
@@ -72,105 +65,6 @@ INT8 get_free_sprite(){
 	}
 }
 
-
-void LCD_Interrupt(void){
-
-	
-	/*
-	
-	
-	if(LY_REG < 120u) {
-		
-		SCX_REG = 0x00;
-	}else{
-		
-		SCX_REG = effect[counter];		
-		
-		if(counter != 64u){
-			
-			counter+=1;
-		}else{
-			counter = 0;
-		}
-		
-		
-	}
-	
-	//SCX_REG = 0x00;
-	
-*/
-}
-
-
-INT16 map_down_collision(INT16 x, INT16 y){
-	INT16 a, b, x2;
-	
-	x2 = x + 0x06;
-	x = x - 0x06;
-	x = x >> 3;
-	x2 = x2 >> 3;
-	
-	y = y + 0x0D;
-	y = y >> 3;
-			
-	index = ((mapSize * y) + x);
-	a = map[index];
-	index2 = ((mapSize * y) + x2);
-	b = map[index2];
-	
-	if( IS_SOLID(a) || IS_SOLID(b)){
-		return 1;
-	}else{
-		return 0;
-	}
-}
-
-INT16 map_collision(INT16 x, INT16 y, UINT8 dir){
-	INT16 a, b; 
-	x = x - 0x08;
-	switch (dir) {
-		case 0x08:
-			y = y - 0x0D;
-		break;
-		
-		case 0x06: //right
-			//x-= 0x01;
-			//x+= xsp;
-			b = 0x02;
-			a = 0x02;
-			
-		break;
-		
-		case 0x02: //down
-		break;
-		
-		
-		case 0x04: //left			
-			//x-= xsp;
-			a = 0x00;
-			b = 0x00;
-			
-		break;
-	}
-	
-	
-	x = x >> 3;
-	y = y >> 3;
-	
-	
-	index = ((mapSize * y) + x);
-	
-	
-	a = map[index + a];
-	b = map[(index + b) +  mapSize];
-	
-	if( IS_SOLID(a) || IS_SOLID(b)  ){
-		return 1;
-	}else{
-		return 0;
-	}
-}
-
 void fx_jump(){
 	NR10_REG = 0x77;
 	NR11_REG = 0x41;
@@ -182,15 +76,16 @@ void fx_jump(){
 void draw_player(){	
 	
 	if(sflip){
-		set_sprite_tile(2, sindex);
-		set_sprite_tile(3, sindex + 0x01);	
-		set_sprite_tile(0, sindex + 0x02);
-		set_sprite_tile(1, sindex + 0x03);		
+		set_sprite_tile(2, sprite_index);
+		set_sprite_tile(3, sprite_index + 0x01);	
+		set_sprite_tile(0, sprite_index + 0x02);
+		set_sprite_tile(1, sprite_index + 0x03);		
 	}else{	
-		set_sprite_tile(0, sindex);
-		set_sprite_tile(1, sindex + 0x01);	
-		set_sprite_tile(2, sindex + 0x02);
-		set_sprite_tile(3, sindex + 0x03);
+		set_sprite_tile(0, sprite_index);
+		set_sprite_tile(1, sprite_index + 0x01);	
+		set_sprite_tile(2, sprite_index + 0x02);
+		set_sprite_tile(3, sprite_index + 0x03);
+
 	}
 	set_sprite_prop(0, sflip);
 	set_sprite_prop(1, sflip);
@@ -201,53 +96,36 @@ void draw_player(){
 
 void checkInput() {
 	oldjoystate = joystate;
-	joystate = joypad();
-	//sflip = 0x00;
-	player_direction = 0x00;
-	///sindex = 0x00;
+	joystate = joypad();	
+	player_direction = 0x00;	
 	sflip = 0x00;
-	sindex = soffset;
+	sprite_index = 0x00;
 		
 	
-	if(joypad()){	
-
-		
+	if(joypad()){
 		if (ISDOWN(J_LEFT)){			
 			sflip= 0x20;
 			player_direction = 0x04;
-			sindex= 0x10;	
-			
-			if(map_collision(xpos, ypos, 0x04)){
-				//xsp = 0x00;
-			}else{
-				xpos-=  0x02;							
+			sprite_index= 0x10;
+
+			if(xpos != 8u){
+				xpos-= 0x02;	
 			}
 			
-			
-			
+
 		}
 	
 		if (ISDOWN(J_RIGHT)){			
 				sflip+= 0x00;
 				player_direction = 0x06;
-				sindex= 0X10;
-				
-			if(map_collision(xpos, ypos, 0x06)){
-				///xsp = 0x00;
-			}else{
-				xpos+= 0x02;							
-			}
-			
-			
-			/*if(xpos != 0x98){				
-				if(!map_collision(xpos, ypos, 0x02)){
-					xpos+= speed_movement;
-				}
-			}*/
+				sprite_index= 0X10;
+				if(xpos != 136u){
+					xpos+= 0x02;	
+				}	
 		}
 		
 		if (ISDOWN(J_DOWN)){								
-			sindex= 0X18;		
+			sprite_index= 0X18;		
 			player_direction = 0x02;
 		}
 	
@@ -270,160 +148,93 @@ void checkInput() {
 		
 		
 		if (ISDOWN(J_A)){
-			
-			if(player_direction == 0x02){
-				if(ypos != 0x18){
-					if( ysp == 0x00){
+				
+					//if( ysp == 0x00){
 						rshoot = 0x01;
 						lshoot = 0x00;
 						dshoot = 0x01;
 						ysp = jump_force;
 						fx_jump();
-					}
-				}
-			}
-			
-			/*
-			
-			if(player_direction == 0x06){
-				if(xpos != 0x10){
-					if( xsp == 0x00){
-						rshoot = 0x01;
-						lshoot = 0x00;
-						dshoot = 0x00;
-						xsp = shoot_force;
-						fx_jump();						
-					}				
-					
-				}			
-			}
-			
-			if(player_direction == 0x04){
-				if(xpos != 0x98){
-					if( xsp == 0x00){						
-							lshoot = 0x01;
-							rshoot = 0x00;
-							dshoot = 0x00;
-							xsp = shoot_force;
-							fx_jump();
-						
-					}				
-					
-				}			
-			}
-			*/
-			
+					//}
+				
 		} 		
 		
 		
 	}else{
 		if(onGround){
 			
-			draw_player();
+			//draw_player();
 			
 		}
 		
 	}	
 }
-	
+
+void water_movement(){
+	if(water_delay > 0x04){
+		water_delay = 0x00;
+		if(water_animation < 512u){			
+			memcpy(0x9580, &water[water_animation], sizeof(unsigned char) * 16);
+			memcpy(0x95A0, &water[water_animation + 16u ], sizeof(unsigned char) * 16);
+			memcpy(0x9590, &water[water_animation + 32u ], sizeof(unsigned char) * 16);
+			memcpy(0x95B0, &water[water_animation + 48u ], sizeof(unsigned char) * 16);
+			water_animation+= 64u;			
+		}else{
+			water_animation = 0;
+			water_delay = 0x05;
+		}
+	}else{
+		water_delay+= 0x01;
+	}
+}	
 
 void game_play(){
-	if(stepCounter > 0x00){			
-		stepCounter = 0x00;
-		
-		
-		/*
-		animate+= 0x01;
-		if(animate > 0x08){			
-			soffset = 0x04;
-			
-			
-			memcpy(0x91E0, 0x91F0, sizeof(unsigned char) * 16);
-			memcpy(0x9210, 0x9220, sizeof(unsigned char) * 16);
-			
-		}else{
-			//animate = 0x00;
-			soffset = 0x00;
-			memcpy(0x91E0, 0x9200, sizeof(unsigned char) * 16);
-			memcpy(0x9210, 0x9230, sizeof(unsigned char) * 16);
-		}
-		if(animate == 0x0f){
-			animate = 0x00;
-		}
-		*/
-		
+	if(game_play_counter > 0x00){			
+		game_play_counter = 0x00;
+		 water_movement();
 		checkInput();
-		onGround = map_down_collision(xpos, ypos);
-		if (ypos > 0x8F){
-			ypos = 0x10;
+		onGround = 0;
+		if (ypos > 128u){
+			ypos = 14u;
 		}
-			
-		
 		
 		if(ysp != 0x00){
 			 ysp-= 0x01;
 		}
 		
 		if(dshoot){
-			if( ypos > 0x17){
+			if( ypos > 16u){
 				ypos-= ysp;
 				move_sprite(0x04, xpos, ypos + 16U);
 			}
 		}
 		
-		/*
-		if(xsp != 0x00){
-			 xsp-= 0x01;
-		}
-		
-		
-		
-		
-		if(player_direction){
-			if(map_collision(xpos, ypos, 0x06)){
-				xsp = 0x00;
-			}else{
-				xpos+= xsp;							
-			}
-		}
-			
-		if(rshoot){
-			if(map_collision(xpos, ypos, 0x04)){
-				xsp = 0x00;
-			}else{
-				xpos-= xsp;							
-			}
-		}
-				
-		*/
-		
-		
-		
-		
 		
 		if(onGround){
-			//ysp = 0x00;
-			//can_jump = 0x00;
-			//ypos-=gravity;			
+		
 		}else{
 			ypos+= gravity;
 			
 		}
 
-		draw_player();
-		move_sprite(0, xpos - 0x08, ypos);
-		move_sprite(1, xpos - 0x08, ypos + 0x08);
-		move_sprite(2, xpos, ypos );
-		move_sprite(3, xpos, ypos + 0x08);
+		//draw_player();
+		move_sprite(0, xpos, ypos);
+		move_sprite(1, xpos + 8u, ypos);
+		move_sprite(4, xpos + 16u, ypos);
+		move_sprite(5, xpos + 24u, ypos);
 
+		ypos2 = ypos + 16u;
+		move_sprite(2, xpos, ypos2);
+		move_sprite(3, xpos + 8u, ypos2);
+		move_sprite(6, xpos + 16u, ypos2);
+		move_sprite(7, xpos + 24u, ypos2);
+
+
+
+			
 		
-			
-			
-				
-		
-			
 	}else{			
-		stepCounter+= 0x01;
+		game_play_counter+= 0x01;
 	}
 }
 
@@ -436,24 +247,20 @@ void game_boot() {
 	HIDE_WIN;
     HIDE_BKG;
     STAT_REG = 8;
+    SPRITES_8x16;
     
     //disable_interrupts();
     //add_LCD(LCD_Interrupt);
 	//add_VBL(VBL_Interrupt);
     //enable_interrupts();
-    
-   
 	
 	/*if((_cpu == 0x01 )  || (_cpu == 0xFF)){
 	  BGP_REG = 0x01B; //classic and pocket	
 	}*/
 	
     
-
-	
 	gravity = 0x03;
-	jump_force = 0x0A;
-	shoot_force = 0x04;
+	jump_force = 0x08;	
 	speed_movement = 0x02;
     xpos = 0x58;
     ypos = 0x10;
@@ -461,21 +268,15 @@ void game_boot() {
 	xsp = 0x00;
 	sflip = 0x00;
 	can_jump = 0x01;
-	
-	
+		
 	//bank, sample,song
-	CP_LoadMusic(2, 0, 0 );
-	
-	//set_interrupts(LCD_IFLAG | VBL_IFLAG );
-    
+	CP_LoadMusic(2, 0, 0 );	
+	//set_interrupts(LCD_IFLAG | VBL_IFLAG );    
     //SHOW_BKG;
-    //SHOW_WIN;
-	
-	//draw_player(0x00);
-	
+    //SHOW_WIN;	
 	//HIDE_BKG;
-	set_bkg_data(0, 0x47, logo_tile_data);
-    set_bkg_tiles(0, 0, 20, 18, logo_map_data);
+	set_bkg_data(0, oldrobotto_tile_count, &oldrobotto_tile_data);
+    set_bkg_tiles(0, 0, 20, 18, &oldrobotto_map_data);
 	SHOW_BKG;
 	SHOW_SPRITES;
 }
@@ -488,34 +289,31 @@ void game_intro(){
 	game_status = GAME_TITLE;	
 	wait_vbl_done();
 	HIDE_BKG;
-	set_bkg_data(0, 0xF3, title_tile_data);
-    set_bkg_tiles(0, 0, 20, 18, title_map_data);  
-	SHOW_BKG;
+	set_bkg_data(0, background_tile_count, background_tile_data);
+    set_bkg_tiles(0, 0, 20, 18, background_map_data);
+	SHOW_BKG;	
+	//SCX_REG = 0x08;
+	//SCY_REG = 0x0F;			
+
+	set_sprite_data(0, 16, &player);
+	set_sprite_tile(0, 0);
+	set_sprite_tile(1, 2);
+	set_sprite_tile(2, 4);
+	set_sprite_tile(3, 6);
+
+	set_sprite_tile(4, 8);
+	set_sprite_tile(5, 10);
+	set_sprite_tile(6, 12);
+	set_sprite_tile(7, 14);
+
+	OBP0_REG = 0xC4; //11 00 01 00
+	
+
+	game_status = GAME_PLAY;	
+	//CP_Play();
 }
 
 game_title(){
-	
-	wait_vbl_done();
-	
-	if(joypad){
-		
-		if (joypad() & J_START){
-				HIDE_BKG;
-		set_bkg_data(0, 127, maptiles);
-		set_bkg_tiles(0, 0, 22, 21, map);    
-		set_sprite_data(0, 127u, face);
-		SHOW_BKG;
-		SCX_REG = 0x08;
-		SCY_REG = 0x0F;
-		CP_Play();	
-		game_status = GAME_PLAY;
-		}
-		
-	}
-		
-	
-
-
 }
 
 void main() {
@@ -523,6 +321,8 @@ void main() {
 	
 	while (1) {		
 		wait_vbl_done();
+		CP_UpdateMusic();
+		
 		
 			
 		switch (game_status){
@@ -544,7 +344,6 @@ void main() {
 			
 			case GAME_PLAY:				
 				game_play();
-				CP_UpdateMusic();
 				
 			break;
 			
